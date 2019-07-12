3f0ada741d04c08284c0604abd7f98812b6f9a05
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-07-12 12:02:26 -0400

    Header Component complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,32 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-container component
 
-function Header() {}
+
+function Header() {
+    // creating elements:
+    const header = document.createElement('div');
+    const span1 = document.createElement('span');
+    const head1 = document.createElement('h1');
+    const span2 = document.createElement('span');
+    
+    // adding classes
+    header.classList.add('header');
+    span1.classList.add('date');
+    span2.classList.add('temp');
+
+
+    // adding text
+    span1.textContent = 'March 28, 2019';
+    head1.textContent = 'Lambda Times';
+    span2.textContent = '98°';
+    // appending elements
+    header.appendChild(span1);
+    header.appendChild(head1);
+    header.appendChild(span2);
+
+    return document.querySelector('.header-container').appendChild(header);
+}
+Header();
+
